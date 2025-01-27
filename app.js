07d1618bfbba5241167309566815628248aeb169
edson suraty de andrade MINSAIT <edson.andrade_minsait@light.com.br>
2025-01-27 17:40:46 -0300

    feat: Create a list of friends, store names in the list and clear the field after writing

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+
+function adicionarAmigo(){
+
+    let amigo = document.querySelector('input').value;
+    listaDeAmigos.push(amigo);
+    limparCampo();
+
+    console.log(listaDeAmigos);
+}
+
+function limparCampo(){
+    amigo = document.querySelector('input');
+    amigo.value = '';
+};
