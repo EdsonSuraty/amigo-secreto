e60aab0cb142a4c1faf0670386436b3a2c36488d
edson suraty de andrade MINSAIT <edson.andrade_minsait@light.com.br>
2025-03-07 10:39:51 -0300

    feat: add function atualizarLista

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ function adicionarAmigo(){
     let amigo = document.getElementById('amigo').value.trim();
     
     if (amigo === ""){
-        alert('É necessário escrever um nome!!')
+        alert('Por favor, insira um nome válido!');
         console.log('Entrada inválida. Nenhum amigo adicionado');
     } else if (listaDeAmigos.includes(amigo)) {
         alert('Esse nome já foi adicionado!');
         console.log('Entrada repetida. Nenhum amigo adicionado')
     } else {
         listaDeAmigos.push(amigo);
+        atualizarLista();
         console.log(`Amigo ${amigo} adicionado`);
         console.log(listaDeAmigos)
     }
@@ -21,6 +22,17 @@ function adicionarAmigo(){
 }
 
 function limparCampo(){
-    campo = document.querySelector('input');
+    let campo = document.querySelector('input');
     campo.value = '';
 };
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let amigo of listaDeAmigos) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li); 
+    }
+}
